0fdee6063331c2da724f990d32c8dedfee650e62
Katrinasms <katrinashui1992@gmail.com>
2023-01-31 16:33:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(errorHandler)
 
 
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5001 ;
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
